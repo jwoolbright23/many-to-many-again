fbb21045da9503248f9a8c89c3f4634abebfab21
Courtney Frey <courtneyb.frey@gmail.com>
2022-10-13 14:47:51 -0500

    project started and notes added

---
+++ CodingEventsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingEventsMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

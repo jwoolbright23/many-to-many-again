58b3d01b70ceb0f2e03c66461c31d78e2ee8421f
Courtney Frey <courtneyb.frey@gmail.com>
2022-11-03 10:32:26 -0500

    working through chap 13

---
+++ CodingEventsMVC/Controllers/EventsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsMVC.Controllers
+{
+    //13.6.3.1 
+    public class EventsController : Controller
+    {
+        //13.10 - convert list to dictionary
+        //static private List<string> Events = new List<string>();
+
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        //13.6.3.2
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //13.6.3.3
+            //Comment these out -- NewEvent method replaces this
+            //Events.Add("Code With Pride");
+            //Events.Add("Strange Loop");
+            //Events.Add("Code & Coffee");
+
+            //13.6.3.4
+            ViewBag.events = Events;
+
+            return View();
+
+        }
+
+        //13.6.4
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            return Redirect("/Events");
+        }
+    }
+}
+

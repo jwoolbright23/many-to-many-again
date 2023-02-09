211df507c00ca5f7a27d7580ed18a050cc9066fe
John Woolbright <jwoolbright23@gmail.com>
2023-02-09 13:44:22 -0600

    maybe working fix?

--- CodingEventsMVC/Program.cs
+++ CodingEventsMVC/Program.cs
@@ -14,10 +14,11 @@ builder.Services.AddControllersWithViews();
     // working with the .NET 6 (specifically the lack of a Startup.cs)
         //https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples?view=aspnetcore-6.0#add-configuration-providers
 
-    var connectionString = "server=localhost;user=testingMVC;password=testingMVC;database=testingMVC";
+    var connectionString = "Server=localhost;Port=3306;Uid=testingMVC;Pwd=testingMVC;Database=testingMVC";
     var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
 
     builder.Services.AddDbContext<EventDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
+
 //--- end of connection syntax
 
 # endregion
@@ -31,6 +32,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<EventDbContext>();
+    dataContext.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
